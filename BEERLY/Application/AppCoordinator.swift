91d66bf9519b61f1a2d4dbecac910ed0d4585f9c
aadxtn <zhansuluu.kydyrova@bk.ru>
2023-07-10 01:13:34 -0400

    Initial commit

---
+++ BEERLY/Application/AppCoordinator.swift
@@ -0,0 +1,25 @@
+//
+//  AppCoordinator.swift
+//  BEERLY
+//
+//  Created by Zhansuluu Kydyrova on 10/7/23.
+//
+
+import UIKit
+
+final class AppCoordinator {
+    
+    private let window: UIWindow?
+    
+    init(window: UIWindow?) {
+        self.window = window
+    }
+    
+    func start() {
+        openRootViewController()
+    }
+    
+    private func openRootViewController() {
+        window?.rootViewController = ViewController()
+    }
+}
